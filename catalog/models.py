34e6ccffce891aae79b64b68d2e3f0c3a2eef667
Omer Taha <sudani.coder@gmail.com>
2024-04-16 23:55:00 +0400

    create database models for the library

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.db import models # Imports the models module, which contains the model base class
+from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
+
+from django.db.models import UniqueConstraint # Constrains fields to unique values
+from django.db.models.functions import Lower # Returns lower cased value of field
 
 # Create your models here.
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('genre-detail', args=[str(self.id)])
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    pass
+
+class Book(models.Model):
+    pass
+
+class BookInstance(models.Model):
+    pass
+
+class Author(models.Model):
+    pass
